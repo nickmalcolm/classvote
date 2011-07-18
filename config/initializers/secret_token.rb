929b74ffb4d595ee2e01e45829e8fa87ea286dd0
Nick Malcolm <nick@southgatelabs.com>
2011-07-18 14:39:15 +1200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Classvote::Application.config.secret_token = '9172625d2a3e5c28847728973740ad6d567cb6d2fc79bf116fdc599a82d7649739f9bd8564a1ffaa810640cb31e2f2467bcfe8ed4d50abdada5ae00a29da376a'
