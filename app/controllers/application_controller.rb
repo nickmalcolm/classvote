7e0196b93335a915bcb56a3e7925b4e93a10ae9b
Nick Malcolm <nick@southgatelabs.com>
2011-07-18 22:58:07 +1200

    Set current student in app controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  
+  before_filter :set_current_student
+  
+  private
+  def set_current_student
+    @current_student = session[:student_id_number]
+  end
+  
 end
