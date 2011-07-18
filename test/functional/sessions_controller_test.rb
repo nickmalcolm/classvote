0322ffb581afcec81451ef1dea0fe5d38ebc6663
Nick Malcolm <nick@southgatelabs.com>
2011-07-18 22:36:37 +1200

    Failing session tests

---
+++ test/functional/sessions_controller_test.rb
@@ -0,0 +1,23 @@
+require 'test_helper'
+
+class SessionsControllerTest < ActionController::TestCase
+  
+  test "can get new" do
+    get :new
+    assert_response :success
+    assert_nil session[:student_id_number]
+  end
+  
+  test "can post student id to create session" do
+    post :create, :student_id_number => 300170288
+    assert_response :success
+    assert_not_nil session[:student_id_number]
+  end
+  
+  test "can destroy session" do
+    session[:student_id_number] = 123456
+    get :destroy
+    assert_nil session[:student_id_number]
+  end
+  
+end
