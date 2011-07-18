5318defe830e80f5b55e7ca68d01db0b26e5cc24
Nick Malcolm <nick@southgatelabs.com>
2011-07-19 02:17:27 +1200

    Form nicities

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  
+  def valid_tags
+    %w(a p strong em b ul ol li)
+  end
+  
+  def super_sanitize(dirty)
+    sanitize(dirty, :tags => valid_tags, :attributes=>"href")
+  end
+  
 end
