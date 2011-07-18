a92772dded61ad73d3f3ae2c7cc6629d3337d45b
Nick Malcolm <nick@southgatelabs.com>
2011-07-19 02:23:40 +1200

    Show votes left in title bar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,13 @@
 module ApplicationHelper
   
+  def current_vote_count
+    Vote.where("student_id_number = ?", @current_student).count
+  end
+  
+  def votes_left
+    3 - current_vote_count
+  end
+  
   def valid_tags
     %w(a p strong em b ul ol li)
   end
--- app/helpers/choices_helper.rb
+++ app/helpers/choices_helper.rb
@@ -2,7 +2,7 @@ module ChoicesHelper
   
   def can_vote_on(choice)
     unless @current_student.nil?
-      Vote.where("student_id_number = ?", @current_student).count < 3
+      current_vote_count < 3
     end
   end
   
