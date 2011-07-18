28b00cdfa30f010eb4d0724bfa8d74e953f5e888
Nick Malcolm <nick@southgatelabs.com>
2011-07-18 23:21:37 +1200

    Three votes per student

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -2,4 +2,15 @@ class Vote < ActiveRecord::Base
   
   validates_presence_of :student_id_number
   
+  validate :three_vote_limit
+  
+  private
+  def three_vote_limit
+    count = Vote.where("student_id_number = ?", self.student_id_number).count
+    
+    if count >= 3
+      self.errors.add :student_id_number, " has already voted 3 times"
+    end
+  end 
+  
 end
--- test/unit/vote_test.rb
+++ test/unit/vote_test.rb
@@ -10,4 +10,18 @@ class VoteTest < ActiveSupport::TestCase
     assert Vote.new(:student_id_number => 12345).valid?
   end
   
+  test "student id can vote thrice" do
+    3.times do
+      assert Vote.create!(:student_id_number => 12345)
+    end
+  end
+  
+  test "student id can't vote four times" do
+    3.times do
+      assert Vote.create!(:student_id_number => 12345)
+    end
+    
+    assert Vote.new(:student_id_number => 12345).invalid?
+  end
+  
 end
